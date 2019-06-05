95eb32f39da39a50bf72fd5fc0d1c6c5163dc1f3
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-06-04 23:01:37 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+window.onload = init() 
+
+function init() {
+    let httpRequest
+    
+    function makeRequest() {
+        httpRequest = new XMLHttpRequest()
+
+        if (!httpRequest) {
+            alert("Giving up! Cannot create an XMLHTTP instance")
+        }
+         
+        httpRequest.onreadystatechange = processContents
+        httpRequest.open("GET", "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random")
+        httpRequest.setRequestHeader("X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com")
+        httpRequest.setRequestHeader("X-RapidAPI-Key", "efcdc68c39msha464a459921fc2bp1e6573jsnf677d17e2212")
+        httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+        httpRequest.send() 
+    }
+
+    function processContents() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                let data = httpRequest.responseText
+
+                if (data) {
+                    data = JSON.parse(data)
+                    console.log(data)
+                    if (data.value) createCards(data.value)
+                }
+            } else {
+                alert("There was a problem with request")
+            }
+        }
+    }
+
+    function createCards(items) {
+        let cardDeck = document.querySelector(".container > .card-deck")
+        let cards = ``
+        
+        for (let item in items) {
+            if (item.hasOwnProperty(item)) {
+                cards += `<div class="card mb-4 shadow-sm">
+                                <div class="card-header">
+                                    <h4 class="my-0 font-weight-normal">${items}</h4>
+                                </div>
+                                <div class="card-body">
+                                    <h1 class="card-title pricing-card-title">${items}</h1>
+                                </div>
+                            </div>`
+            }
+        }
+
+        cardDeck.innerHTML = cards
+    }
+
+    initialRequest()
+}
+
